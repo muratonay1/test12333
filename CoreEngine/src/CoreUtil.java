1c7dc91f26f9c6498117738fda3a316709b34e3f
İsmet Murat ONAY <34923740+muratonay1@users.noreply.github.com>
2022-12-10 23:58:29 +0300

    test commit intellij

---
+++ CoreEngine/src/CoreUtil.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class CoreUtil {
+    public ArrayList<String> getAlive(){
+        ArrayList<String> list = new ArrayList<>();
+        list.add("murat");
+        return list;
+    }
+}
---
+++ CoreEngine/src/EngineStarter.java
@@ -0,0 +1,9 @@
+
+
+public class EngineStarter extends CoreUtil{
+    public static void main(String[] args) {
+
+        System.out.println("Engine running successfully.");
+        System.out.println(new CoreUtil().getAlive());
+    }
+}
