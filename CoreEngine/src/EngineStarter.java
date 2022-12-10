6d176614de920833d0c73a87fcce798a724cbd27
İsmet Murat ONAY <34923740+muratonay1@users.noreply.github.com>
2022-12-11 00:10:37 +0300

    dev push

--- CoreEngine/src/EngineStarter.java
+++ CoreEngine/src/EngineStarter.java
@@ -3,7 +3,7 @@
 public class EngineStarter extends CoreUtil{
     public static void main(String[] args) {
 
-        System.out.println("Engine running successfully.");
+        System.out.println("Engine running successfully.123");
         System.out.println(new CoreUtil().getAlive());
     }
 }
